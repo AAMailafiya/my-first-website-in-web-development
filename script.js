065a6f2d12dda18d344c578316273bae979914c0
AAMailafiya <abubakarabdullahimailafiya2023@gmail.com>
2025-03-19 11:45:20 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let firstname ="john"
+let age ="52"
+console.log(firstname,age)
+let obj= {
+    name:"john",
+    age:24,
+    address:"government are"
+}
+let arr=[2,8,"musa","man",true]
+console.log(obj)
+
+const ages = 15
+if( ages >= 18){
+    console.log ("adult")
+}else{
+    console.log("minor")
+}
+
+function greet(){
+    alert("good morning")
+}
